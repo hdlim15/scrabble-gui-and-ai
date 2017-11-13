d160400bd429ae1dfdbfd81513524a8ff28e636d
Connor Aidan McHugh <cam462@cornell.edu>
2017-11-13 17:42:00 -0500

    ai tests

--- test_main.ml
+++ test_main.ml
@@ -1,7 +1,7 @@
 open OUnit2
 
 let suite = "Scrabble test suite" >:::
-  Test_trie.tests @ Test_command.tests @ Test_state.tests
+  Test_trie.tests @ Test_command.tests @ Test_state.tests @ Test_ai.tests
 
 (* The following line must be the one and only place
  * in your entire source code that calls [OUnit2.run_test_tt_main]. *)
