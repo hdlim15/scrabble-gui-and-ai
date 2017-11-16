54672484896534f79a48541111f2b09e530595eb
Hyuckin David Lim <hdl39@cornell.edu>
2017-11-15 19:47:05 -0500

    tentatively finished first implementation of trie

--- trie.ml
+++ trie.ml
@@ -18,12 +18,13 @@ let remove_child n dict =
          | Node (c', _, _) -> c' <> c
         ) dict
 
+(* [add_child n dict] is [dict] with [n] appended as a child. If the character
+ * of [n] is already contained in a direct child of [dict], [n] replaces that
+ * child. *)
 let add_child n dict =
   match dict with
   | Node (c, lst, so) -> Node (c, n::(remove_child n lst), so)
 
-(* [get_subtree c dict] is None, if [c] is not the char of a child of [dict].
- * If [c] is the char of a child of dict, it returns Some child *)
 let get_subtree c dict =
   match dict with
   | Node (_, lst, _) ->
@@ -32,7 +33,6 @@ let get_subtree c dict =
         | Node (c', _, _) -> c' = c
       ) lst
 
-(* todo: adding a prefix as a word needs to be marked as a word *)
 let insert dict w =
   let char_list = explode w in
   let rec helper lst acc =
@@ -54,39 +54,39 @@ let insert dict w =
   in
   helper char_list dict
 
-let insert_word_list lst =
-  (* [+] used to represent the head of the trie *)
-  let root = Node ('+', [], false) in
-  List.fold_left insert root lst
-
-
 let is_word dict w =
   let char_list = explode w in
   let rec helper lst dict =
     match lst with
     | [] -> false
-    | c :: [] ->
-      begin
-        match get_subtree c dict with
-        | None -> false
-        | Some (Node(_, _, b)) -> b
-      end
     | c :: t ->
       begin
         match get_subtree c dict with
         | None -> false
-        | Some d' -> helper t d'
+        | Some d' ->
+            if (t = []) then let (Node(_, _, b)) = d' in b
+            else (helper t d')
       end
   in
   helper char_list dict
 
-(*
-             *
-            /
-+ - h - e - l - l - o - *
-     \
-      a
-       \
-        t - e - *
-         \
-          * *)
+(* [insert_word_list lst] is the dictionary that results from adding all the
+ * words in [lst] to the empty dictionary. *)
+let insert_word_list lst =
+  (* [+] used to represent the head of the trie *)
+  let root = Node ('+', [], false) in
+  List.fold_left insert root lst
+
+(* [initialize_dict file] is the dictionary that results from adding all the
+ * words in [file] to the empty dictionary.
+ * requires: there is one word per line in [file]
+ * aside: if you uncomment the commented code, it will also return a list of
+ * all the words, which is useful for utop debugging *)
+let initialize_dict file =
+  let rec helper channel dict (* lst *) =
+    match (Pervasives.input_line channel) with
+    (* If End_of_file, close file and return idx' *)
+    | exception End_of_file -> Pervasives.close_in channel ; dict(* , lst *)
+    | s -> helper channel (insert dict s) (* (s::lst) *)
+  in
+  helper (Pervasives.open_in file) (Node ('+', [], false)) (* [] *)
--- trie.mli
+++ trie.mli
@@ -1,7 +1,9 @@
 (* The type of the dictionary, which will be implemented later as a trie *)
 type dictionary = Node of char * (dictionary list) * bool
 
-(* [get_subtree c dict] is the subtree of [dict] with next letter [c] *)
+(* [get_subtree c dict] is [None] if no direct child of [dict] contains [c].
+ * If there is a direct child of [dict], [child], that contains [c],
+ * it returns [Some child] *)
 val get_subtree : char -> dictionary -> dictionary option
 
 (* [insert w dict] is [dict] with [w] appended as a valid word in [dict] *)
