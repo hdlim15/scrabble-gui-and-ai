4d6a6c206baed7c7bd0d8162963229d3e8d3ce12
Arman Vaziri <adv28@cornell.edu>
2017-11-21 14:20:38 -0500

    more advanced place tests

--- test_state.ml
+++ test_state.ml
@@ -107,15 +107,18 @@ let rec get_prev_player n p =
   in
   List.hd (List.filter (fun p' -> p'.order_num = n') p)
 
-(* checks all players have 7 letters in rack *)
+(* [check_racks players] checks that all [players] have 7 letters in rack *)
 let rec check_racks players =
   List.for_all (fun p -> List.length p.rack = 7) players
 
-(* checks all players have unique order nums *)
+(* [check_order_nums players] checks that all [players] have unique order
+ * numbers. *)
 let rec check_order_nums players =
   let order_nums = List.fold_left (fun acc p -> p.order_num :: acc) [] players in
   List.length (List.sort Pervasives.compare order_nums) = List.length order_nums
 
+(* [verify_board c st is_h word] returns true if the word placed at coordinate
+ * [c] in the direction specified by [is_h] is equal to [word]. *)
 let verify_board c st is_h word =
   match (get_adjacent_word c st is_h []) with
   | Some (s, _) -> s = word
@@ -218,7 +221,8 @@ let it_no_player = {blank_player with rack = [('i',1); ('t',1); ('n',1); ('o',1)
 
 
 let blank1_state = {board = init_board 15;
-                    bag = [('f',4);('f',4);('f',4);('f',4);('f',4);('f',4);('f',4);('f',4);('f',4);('f',4);('f',4);('f',4);('f',4);('f',4);];
+                    bag = [('f',4);('f',4);('f',4);('f',4);('f',4);('f',4);
+                           ('f',4);('f',4);('f',4);('f',4);('f',4);('f',4);('f',4);('f',4);];
                     players = [blank_player];
                     added_words = [];
                     current_player = blank_player}
@@ -238,11 +242,14 @@ let basic_mv4 = {word=['c';'a';'m';'e';'l';'s'];mv_coord=(7,7);is_horizontal=tru
 let basic_mv5 = {word=['c';'a';'m';'e';'l';'s'];mv_coord=(7,7);is_horizontal=false}
 let basic_mv6 = {word=['c';'a';'m';'e';'l'];mv_coord=(4,10);is_horizontal=false}
 
-let it_mv1 = {word=['i';'t'];mv_coord=(7,7);is_horizontal=true}
-let no_mv1 = {word=['n';'o'];mv_coord=(8,7);is_horizontal=true}
-let it_mv2 = {word=['i';'t'];mv_coord=(7,7);is_horizontal=false}
-let no_mv2 = {word=['n';'o'];mv_coord=(7,8);is_horizontal=false}
-
+let it_mv1_h = {word=['i';'t'];mv_coord=(7,7);is_horizontal=true}
+let no_mv1_h = {word=['n';'o'];mv_coord=(7,7);is_horizontal=true}
+let no_mv1_h_below = {word=['n';'o'];mv_coord=(8,7);is_horizontal=true}
+let it_mv1_h_above = {word=['i';'t'];mv_coord=(6,7);is_horizontal=true}
+let it_mv2_v = {word=['i';'t'];mv_coord=(7,7);is_horizontal=false}
+let no_mv2_v = {word=['n';'o'];mv_coord=(7,7);is_horizontal=false}
+let no_mv2_v_right = {word=['n';'o'];mv_coord=(7,8);is_horizontal=false}
+let it_mv2_v_left = {word=['i';'t'];mv_coord=(7,6);is_horizontal=false}
 
 let camel_hor_st =
   let st = (do' (PlaceWord basic_mv1) basic_state) in
@@ -252,11 +259,51 @@ let camel_vert_st =
   {st with current_player = {camel_player with score=10}}
 
 let it_hor_st =
-  let st = (do' (PlaceWord it_mv1) it_no_state) in
+  let st = (do' (PlaceWord it_mv1_h) it_no_state) in
   {st with current_player = {it_no_player with score=2}}
 let it_vert_st =
-  let st = (do' (PlaceWord it_mv2) it_no_state) in
+  let st = (do' (PlaceWord it_mv2_v) it_no_state) in
   {st with current_player = {it_no_player with score=2}}
+
+let move1_h_invalid = {word = ['s'; 'u'];
+                       mv_coord = (7, 7);
+                       is_horizontal = true}
+
+let move1_v_invalid = {word = ['s'; 'u'];
+                       mv_coord = (7, 7);
+                       is_horizontal = false}
+
+let move_custom_word = {word = ['s'; 'o'; 'p'];
+                        mv_coord = (7, 7);
+                        is_horizontal = true}
+
+let prefix_player = {name = "David";
+                     score = 0;
+                     rack = [('t', 1);('a', 1);('c', 3);('h', 4);('m', 3);('m', 3);('r', 1)];
+                     player_type = Human;
+                     order_num = 1}
+
+let prefix_state = {board = init_board 15;
+                    bag = [('n', 1); ('d', 2); ('h', 4); ('v', 4)];
+                    players = [prefix_player];
+                    added_words = [];
+                    current_player = prefix_player}
+
+let move1_h_hat = {word = ['h'; 'a'; 't'];
+                   mv_coord = (7, 7);
+                   is_horizontal = true}
+
+let move1_v_hat = {word = ['h'; 'a'; 't'];
+                   mv_coord = (7, 7);
+                   is_horizontal = false}
+
+let move2_h_chat = {word = ['c'; 'h'; 'a'; 't'];
+                    mv_coord = (7, 6);
+                    is_horizontal = true}
+
+let move2_v_chat = {word = ['c'; 'h'; 'a'; 't'];
+                    mv_coord = (6, 7);
+                    is_horizontal = false}
 
 let place_tests = [
   (* Blank tile tests *)
@@ -286,9 +333,9 @@ let place_tests = [
   "score_hor_intersect_vert" >:: (fun _ ->
       assert_equal 25 (do' (PlaceWord basic_mv3) camel_vert_st).current_player.score);
   "score_hor_hor" >:: (fun _ ->
-      assert_equal 10 (do' (PlaceWord no_mv1) it_hor_st).current_player.score);
+      assert_equal 10 (do' (PlaceWord no_mv1_h_below) it_hor_st).current_player.score);
   "score_vert_vert" >:: (fun _ ->
-      assert_equal 10 (do' (PlaceWord no_mv2) it_vert_st).current_player.score);
+      assert_equal 10 (do' (PlaceWord no_mv2_v_right) it_vert_st).current_player.score);
   "score_hor_overlap" >:: (fun _ ->
       assert_equal 20 (do' (PlaceWord basic_mv4) camel_hor_st).current_player.score);
   "score_vert_overlap" >:: (fun _ ->
@@ -296,6 +343,7 @@ let place_tests = [
   "score_word_multiplier" >:: (fun _ ->
       assert_equal 28 (do' (PlaceWord basic_mv6) camel_hor_st).current_player.score);
 
+  (* Rack tests. *)
   "place1_rack_horizontal" >:: (fun _ ->
       assert_equal true
         (let st = (do' (PlaceWord move1_h) basic_state_2bag) in
@@ -332,15 +380,6 @@ let place_tests = [
        List.for_all (fun c -> List.mem_assoc c prev_rack) lst_chars
        && List.length prev_rack = 7));
 
-  "place1_bag_horizontal" >:: (fun _ ->
-      assert_equal [] (do' (PlaceWord move1_h) basic_state_2bag).bag);
-
-  "place1_bag_vertical" >:: (fun _ ->
-      assert_equal [] (do' (PlaceWord move1_v) basic_state_2bag).bag);
-
-  "place1_bag_wildcard" >:: (fun _ ->
-      assert_equal 1 (List.length (do' (PlaceWord move1_h) wildcard_player_state).bag));
-
   "place2_rack_horizontal" >:: (fun _ ->
       assert_equal 1
         (let st = {(do' (PlaceWord move1_h) basic_state_2bag)
@@ -353,6 +392,16 @@ let place_tests = [
                    with bag = [('q',10);('a',1);('a',1)]} in
          List.length ((do' (PlaceWord move2_v) st).bag)));
 
+  (* Bag tests. *)
+  "place1_bag_horizontal" >:: (fun _ ->
+      assert_equal [] (do' (PlaceWord move1_h) basic_state_2bag).bag);
+
+  "place1_bag_vertical" >:: (fun _ ->
+      assert_equal [] (do' (PlaceWord move1_v) basic_state_2bag).bag);
+
+  "place1_bag_wildcard" >:: (fun _ ->
+      assert_equal 1 (List.length (do' (PlaceWord move1_h) wildcard_player_state).bag));
+
   "place2_bag_too_small" >:: (fun _ ->
       assert_equal true
         (let st = (do' (PlaceWord move1_h) basic_state_2bag) in
@@ -362,6 +411,7 @@ let place_tests = [
          List.for_all (fun c -> List.mem_assoc c prev_rack) lst_chars
          && List.length prev_rack = 5));
 
+  (* Next player test. *)
   "place2_next_player_overflow" >:: (fun _ ->
       assert_equal 1
         (let st = {(do' (PlaceWord move1_v) basic_state_2bag)
@@ -369,5 +419,77 @@ let place_tests = [
          let st' = (do' (PlaceWord move2_v) st) in
          st'.current_player.order_num));
 
+  (* Board tests. *)
+  "basic_place_horizontal" >:: (fun _ ->
+      assert_equal true (let st = (do' (PlaceWord move1_h) basic_state_2bag) in
+                         verify_board (7, 7) st true "so"));
 
+  "basic_place_vertical" >:: (fun _ ->
+      assert_equal true (let st = (do' (PlaceWord move1_v) basic_state_2bag) in
+                         verify_board (7, 7) st false "so"));
+
+  "place2_horizontal" >:: (fun _ ->
+      assert_equal true
+        (let st = (do' (PlaceWord move1_h) basic_state_2bag) in
+         let st' = (do' (PlaceWord move2_h) st) in
+         verify_board (7, 7) st' true "sort"));
+
+  "place2_horizontal" >:: (fun _ ->
+      assert_equal true
+        (let st = (do' (PlaceWord move1_h) basic_state_2bag) in
+         let st' = (do' (PlaceWord move2_h) st) in
+         verify_board (7, 7) st' true "sort"));
+
+  "place2_vertical" >:: (fun _ ->
+      assert_equal true
+        (let st = (do' (PlaceWord move1_v) basic_state_2bag) in
+         let st' = (do' (PlaceWord move2_v) st) in
+         verify_board (7, 7) st' false "sort"));
+
+  "place_horizontal_invalid" >:: (fun _ ->
+      let e = fun () -> (do' (PlaceWord move1_h_invalid) basic_state_2bag) in
+      assert_raises (InvalidPlace "invalid word") e);
+
+  "place_vertical_invalid" >:: (fun _ ->
+      let e = fun () -> (do' (PlaceWord move1_v_invalid) basic_state_2bag) in
+      assert_raises (InvalidPlace "invalid word") e);
+
+  "place_custom_word" >:: (fun _ ->
+      let st = do' (AddWord "sop") basic_state_2bag in
+      let st' = do' (PlaceWord move_custom_word) st in
+      assert_equal true (verify_board (7, 7) st' true "sop"));
+
+  "place_prefix_horizontal" >:: (fun _ ->
+      let st = (do' (PlaceWord move1_h_hat) prefix_state) in
+      let st' = (do' (PlaceWord move2_h_chat) st) in
+      assert_equal true (verify_board (7, 6) st' true "chat"));
+
+  "place_prefix_vertical" >:: (fun _ ->
+      let st = (do' (PlaceWord move1_v_hat) prefix_state) in
+      let st' = (do' (PlaceWord move2_v_chat) st) in
+      assert_equal true (verify_board (6, 7) st' false "chat"));
+
+  "place_word_below_word" >:: (fun _ ->
+      let st = (do' (PlaceWord it_mv1_h) it_no_state) in
+      let st' = (do' (PlaceWord no_mv1_h_below) st) in
+      assert_equal true ((verify_board (7, 7) st' true "it")
+                         && (verify_board (8, 7) st' true "no")));
+
+  "place_word_above_word" >:: (fun _ ->
+      let st = (do' (PlaceWord no_mv1_h) it_no_state) in
+      let st' = (do' (PlaceWord it_mv1_h_above) st) in
+      assert_equal true ((verify_board (7, 7) st' true "no")
+                         && (verify_board (6, 7) st' true "it")));
+
+  "place_word_right_word" >:: (fun _ ->
+      let st = (do' (PlaceWord it_mv2_v) it_no_state) in
+      let st' = (do' (PlaceWord no_mv2_v_right) st) in
+      assert_equal true ((verify_board (7, 7) st' false "it")
+                         && (verify_board (7, 8) st' false "no")));
+
+  "place_word_left_word" >:: (fun _ ->
+      let st = (do' (PlaceWord no_mv2_v) it_no_state) in
+      let st' = (do' (PlaceWord it_mv2_v_left) st) in
+      assert_equal true ((verify_board (7, 6) st' false "it")
+                         && (verify_board (7, 7) st' false "no")));
 ]
